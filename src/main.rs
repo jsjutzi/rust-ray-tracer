b19aba356283c83a5464461cf9b30b12ad3220fb
Jack Jutzi <jsjutzi@gmail.com>
2022-10-01 17:00:50 -0400

    successful metal sphere trace

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,9 @@ use hit::{Hit, World};
 use rand::{thread_rng, Rng};
 use ray::Ray;
 use sphere::Sphere;
-use std::io::{stderr, Write};
+use std::{io::{stderr, Write}, rc::Rc};
 use vec::{Color, Point3};
+use material::{Lambertian, Metal};
 
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
@@ -42,9 +43,22 @@ fn main() {
 
     // World
     let mut world = World::new();
-    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)));
-    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
+    
+    // Define material properties
+    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let mat_center = Rc::new(Lambertian::new(Color::new(0.7, 0.3, 0.3)));
+    let mat_left = Rc::new(Metal::new(Color::new(0.8, 0.8, 0.8)));
+    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2)));
 
+    let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
+    let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
+    let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
+    let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
+
+    world.push(Box::new(sphere_ground));
+    world.push(Box::new(sphere_center));
+    world.push(Box::new(sphere_left));
+    world.push(Box::new(sphere_right));
     // Camera
     let cam = Camera::new();
 
