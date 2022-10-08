92235b6279daeaf719e4a1835ac356227696c944
Jack Jutzi <jsjutzi@gmail.com>
2022-10-07 20:16:38 -0400

    rough build complete

--- src/material.rs
+++ src/material.rs
@@ -27,7 +27,7 @@ impl Scatter for Lambertian {
         }
 
         let scattered = Ray::new(rec.p, scatter_direction);
-        
+
         Some((self.albedo, scattered))
     }
 }
@@ -47,10 +47,7 @@ impl Metal {
 impl Scatter for Metal {
     fn scatter(&self, r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)> {
         let reflected = r_in.direction().reflect(rec.normal).normalized();
-        let scattered = Ray::new(
-            rec.p,
-            reflected + self.fuzz * Vec3::random_in_unit_sphere(),
-        );
+        let scattered = Ray::new(rec.p, reflected + self.fuzz * Vec3::random_in_unit_sphere());
 
         if scattered.direction().dot(rec.normal) > 0.0 {
             Some((self.albedo, scattered))
